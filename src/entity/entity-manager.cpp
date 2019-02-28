061c01328a4dd13344f74bb4e2838c7464007d6d
C. J. Howard <cjhoward@cjhoward.org>
2019-02-28 14:28:25 +0800

    Update outdated license notice

--- src/entity/entity-manager.cpp
+++ src/entity/entity-manager.cpp
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2015  Christopher J. Howard
+ * Copyright (C) 2017-2019  Christopher J. Howard
  *
- * This file is part of Ecosys.
+ * This file is part of Antkeeper Source Code.
  *
- * Ecosys is free software: you can redistribute it and/or modify
+ * Antkeeper Source Code is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Ecosys is distributed in the hope that it will be useful,
+ * Antkeeper Source Code is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Ecosys.  If not, see <http://www.gnu.org/licenses/>.
+ * along with Antkeeper Source Code.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "entity-manager.hpp"
